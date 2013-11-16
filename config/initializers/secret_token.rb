a4db2ce9c22a8abad1b107e6c4249196ec72d8cb
Rory Crawford <rory@rory-VirtualBox.(none)>
2013-11-16 12:04:51 -0500

    Initial Commit; Built a Rails App; Added a splashpage; it looks beautiful; thanks Marco.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseHbs::Application.config.secret_key_base = 'f92fc0c7f58e5056c7a189eba9e3d10fa96038ab2c4ea399f2f3253db8793e27799ff4d721889688425186b359e81d7d9fcc4743e7771ae6e797c6d56468c93c'
